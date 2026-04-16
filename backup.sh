149c61035034c1b686730cb4566ef57ff98a3f6d
Jon <jb5nnhy@gmail.com>
2026-04-16 12:56:38 +0200

    add: gestione errori

--- backup.sh
+++ backup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+set -euo pipefail #error management
 BACKUP_DIR="$HOME/arch-backup-$(date +%Y%m%d)"
 mkdir -p "$BACKUP_DIR"
 
-echo "=== INZIO BACKUP ==="
+echo "=== INIZIO BACKUP ==="
 echo "1. produzione liste di pacchetti..."
 pacman -Qqen > "$BACKUP_DIR/pkglist-repo.txt" # pacchetti dai repository ufficiali
 pacman -Qqm > "$BACKUP_DIR/pkglist-aur.txt" # pacchetti AUR
@@ -12,7 +13,6 @@ echo "2. produzione archivio dotfiles..."
 tar -czf "$BACKUP_DIR/dotfiles-backup.tar.gz" \
     -C ~/ .bashrc .zshrc .config .local .face .ssh 2>/dev/null
 
-# Aggiungi allo script di backup:
 sudo tar -czf "$BACKUP_DIR/system-configs.tar.gz" /etc/fstab /etc/pacman.conf /etc/default/grub
 
 echo "=== BACKUP COMPLETATO ==="
